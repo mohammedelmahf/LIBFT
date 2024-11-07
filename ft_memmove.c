e7b4b6435bf920614fd167c05b194c750b18b2ff
Mahammed Elmahfoudi <maelmahf@e1r10p1.1337.ma>
2024-11-07 20:18:20 +0100

    125%

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:33:46 by maelmahf          #+#    #+#             */
-/*   Updated: 2024/11/07 17:45:21 by maelmahf         ###   ########.fr       */
+/*   Updated: 2024/11/07 20:14:45 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	d = (unsigned char *)dst;
 	s = (unsigned char *)src;
-	if (!dst || !src)
+	if (!dst && !src)
 		return (NULL);
-	if (d < s || d >= s + len)
-	{
-		while (len--)
-			*(d++) = *(s++);
-	}
-	else
+	if (d > s && d < s + len)
 	{
 		d += len;
 		s += len;
 		while (len--)
 			*(--d) = *(--s);
+	}
+	else
+	{
+		while (len--)
+			*(d++) = *(s++);
 	}
 	return (dst);
 }
